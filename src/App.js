80ded4a4176aeac80cae17b76564aa6c2b620ce6
Francisco Cortez <franciscocortezdev@gmail.com>
2022-05-29 20:54:09 -0600

    Favicon add

--- src/App.js
+++ src/App.js
@@ -1,37 +1,17 @@
-import './App.css';
-import { CountryGame } from './components/CountryGame/CountryGame'
-import {Home} from './Pages/Home/Home'
-import {EndGame} from './Pages/EndGame/EndGame'
-import {Route } from "wouter"
+import "./App.css";
+import { CountryGame } from "./components/CountryGame/CountryGame";
+import { Home } from "./Pages/Home/Home";
+import { EndGame } from "./Pages/EndGame/EndGame";
+import { Route } from "wouter";
 
 export function App() {
- 
-  
- 
   return (
-
-    <div className='container'> 
-      <div className='containerGame'> 
-
-        <Route 
-        component={Home}
-        path="/"
-        />
-        <Route 
-        component={CountryGame}
-        path="/gamestart"
-        />
-        <Route 
-        component={EndGame}
-        path="/gameend/:points"
-        />
-      
+    <div className="container">
+      <div className="containerGame">
+        <Route component={Home} path="/" />
+        <Route component={CountryGame} path="/gamestart" />
+        <Route component={EndGame} path="/gameend/:points" />
       </div>
-
     </div>
-    
-    
-
-
   );
 }
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,22 +1,19 @@
-import React from 'react'
-import './Home.css'
-import startPic from '../../img/StartGame.png'
-import {Link } from "wouter"
+import React from "react";
+import "./Home.css";
+import startPic from "../../img/StartGame.png";
+import { Link } from "wouter";
 
 export function Home() {
   return (
-    <div className='containerHome'>
+    <div className="containerHome">
+      <p className="title">Wellcome</p>
+      <p className="titleTo">To</p>
+      <p className="titleGame">FlagQuiz</p>
 
-      <p className='title'>Wellcome</p>
-      <p className='titleTo'>To</p>
-      <p className='titleGame'>FlagQuiz</p>
-
-     
-      <img className='startPic' src={startPic} alt='startQuiz'/>
-      <Link className='btnStart' to='/gamestart'>Start Quiz</Link>
-
+      <img className="startPic" src={startPic} alt="startQuiz" />
+      <Link className="btnStart" to="/gamestart">
+        Start Quiz
+      </Link>
     </div>
-  )
-
+  );
 }
-
